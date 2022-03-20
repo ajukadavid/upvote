22658278d104888584276a63a90167c06cd1b3f6
Ajuka David <ajuka.zephiniah@gmail.com>
2022-03-20 22:19:30 +0100

    updated onclick handler and sorting with computed properties

--- main.js
+++ main.js
@@ -3,6 +3,19 @@ const upvoteApp = {
         return {
             submissions: Seed.submissions
         }
+    },
+    computed: {
+        sortedSubmissions() {
+            return this.submissions.sort((a, b) => {
+                return b.votes - a.votes
+            })
+        }
+    },
+    methods: {
+        upvote(id){
+            const submission = this.submissions.find((submission) => submission.id === id)
+            submission.votes++
+        }
     }
 }
 
